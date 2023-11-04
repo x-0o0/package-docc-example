1db8927ee17215f9ba578eb387a4b6f94a68b9ad
Jaesung <chic0815@konkuk.ac.kr>
2023-11-04 18:31:39 +0900

    Update GENERATE_DOCS.sh

--- GENERATE_DOCS.sh
+++ GENERATE_DOCS.sh
@@ -1,13 +1,32 @@
 set -e
 
-# 사전 정의
-OUTPUT_DIRECTORY_PATH="./docs"
+# 사전 정의 (Definition)
+OUTPUT_PATH="./docs"
 TARGET_NAME="PackageDocCExample"
 HOSTING_BASE_PATH="package-docc-example"
+# for `xcodebuild docbuild`
+# BUILD_DESTINATION="generic/platform=iOS" 
+# BUILD_PATH="/tmp/docbuild"
+# DOCCARCHIVE_PATH="${BUILD_PATH}/Build/Products/Debug-iphoneos/${TARGET_NAME}.doccarchive"
 
-# 문서 빌드
-swift package --allow-writing-to-directory ${OUTPUT_DIRECTORY_PATH} \
+# ---------------------
+# docc-plugin
+# ----------------------
+swift package --allow-writing-to-directory ${OUTPUT_PATH} \
     generate-documentation --target ${TARGET_NAME} --disable-indexing \
-    --output-path ${OUTPUT_DIRECTORY_PATH} \
+    --output-path ${OUTPUT_PATH} \
     --transform-for-static-hosting \
     --hosting-base-path ${HOSTING_BASE_PATH}
+
+# ---------------------
+# xcodebuild
+# ----------------------
+# xcodebuild docbuild \
+#     -scheme ${TARGET_NAME} \
+#     -destination ${BUILD_DESTINATION} \
+#     -derivedDataPath ${BUILD_PATH}
+#    
+# $(xcrun --find docc) process-archive \
+#     transform-for-static-hosting ${DOCCARCHIVE_PATH} \
+#     --hosting-base-path ${HOSTING_BASE_PATH} \
+#     --output-path ${OUTPUT_PATH}
